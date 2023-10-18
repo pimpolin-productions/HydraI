a67e68a31f1a17c4c9c93c0b90b4907d2cbefe6f
maipley <pablovgv@gmail.com>
2023-10-18 17:51:26 -0300

    Added hydra.live to settings

--- HydraDjango/settings.py
+++ HydraDjango/settings.py
@@ -25,7 +25,7 @@ ALLOWED_IP = os.getenv('ALLOWED_IP')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [ALLOWED_IP]
+ALLOWED_HOSTS = [ALLOWED_IP, 'hydrai.live']
 
 
 # Application definition
